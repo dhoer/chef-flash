d0fcfee4432c66a617f55dbd3e8d8062c0968b27
Dennis Hoer <dennis.hoer@pearson.com>
2016-01-01 22:46:27 -0700

    Fix rubocop offenses

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -11,12 +11,12 @@ end
 case os[:family]
 when 'windows'
   describe file('C:\Windows\SysWOW64\Macromed\FlashPlayerTrust\ChefGeneratedTrust.cfg') do
-    its(:content) { should match /# trust home directory/ }
-    its(:content) { should match /C:\\Users\\vagrant/ }
+    its(:content) { should match(/# trust home directory/) }
+    its(:content) { should match(/C:\\Users\\vagrant/) }
   end
 
   describe file('C:\Windows\SysWOW64\Macromed\Flash\mms.cfg') do
-    its(:content) { should match /AutoUpdateDisable=0/ }
-    its(:content) { should match /SilentAutoUpdateEnable=1/ }
+    its(:content) { should match(/AutoUpdateDisable=0/) }
+    its(:content) { should match(/SilentAutoUpdateEnable=1/) }
   end
 end
