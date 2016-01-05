ed27634da411eb7140151abc759878da89e5c39d
Dennis Hoer <dennis.hoer@pearson.com>
2016-01-04 18:51:21 -0700

    Fix undefined method platform?

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,14 @@
 major_version = node['flash']['version'].split('.')[0]
 system = node['kernel']['machine'] == 'x86_64' ? 'SysWOW64' : 'System32'
 
-# plugin-free ie info: https://msdn.microsoft.com/library/hh968248(v=vs.85).aspx
+# plugin-free ie: https://msdn.microsoft.com/library/hh968248(v=vs.85).aspx
 if platform?('windows')
   windows_package "Adobe Flash Player #{major_version} ActiveX" do
     source "#{node['flash']['download_url']}/#{node['flash']['version']}/"\
       "install_flash_player_#{major_version}_active_x.msi"
     options '/qn'
     installer_type :custom
-    only_if { node['flash']['ie'] && !Flash::IE.flash_preinstalled? }
+    only_if { node['flash']['ie'] && !Flash::IE.flash_preinstalled?(node) }
   end
 
   powershell_script 'Flash is pre-installed but not enabled on Windows Server 2012' do
@@ -16,7 +16,7 @@ if platform?('windows')
       import-module ServerManager
       Add-WindowsFeature -name Desktop-Experience,qWave -IncludeAllSubFeature -IncludeManagementTools
     EOH
-    only_if { node['flash']['ie'] && !Flash::IE.flash_enabled_by_default? }
+    only_if { node['flash']['ie'] && !Flash::IE.flash_enabled_by_default?(node) }
   end
 
   windows_package "Adobe Flash Player #{major_version} NPAPI" do
