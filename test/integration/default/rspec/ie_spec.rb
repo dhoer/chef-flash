edfa53b54ac42140634c20b46e8eaf0c477ce402
Dennis Hoer <dennis.hoer@pearson.com>
2016-01-05 18:20:57 -0700

    Check if Flash is enabled for IE

--- test/integration/default/rspec/ie_spec.rb
+++ test/integration/default/rspec/ie_spec.rb
@@ -9,11 +9,12 @@ describe 'IE Grid', if: WINDOWS do
     @selenium.quit
   end
 
-  it 'Checks if Flash installed' do
+  it 'Checks if Flash installed/enabled' do
     @selenium.get('https://helpx.adobe.com/flash-player.html')
     element = @selenium.find_element(:id, 'displaybutton')
     element.click
     element = @selenium.find_element(:id, 'flashversioninfo')
     expect(element.text).to_not eq('Not installed')
+    expect(element.text).to_not eq('Flash Player disabled')
   end
 end
